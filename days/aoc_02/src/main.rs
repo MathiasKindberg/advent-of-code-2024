5490caa914d58077db79d2c3235cf3e1f5119207
Mathias Kindberg <mathias@kindberg.earth>
2024-12-02 16:01:30 +0100

    day 2 done

--- days/aoc_02/src/main.rs
+++ days/aoc_02/src/main.rs
@@ -1,25 +1,74 @@
 //! Part 1:
 //! Part 2:
 
+use core::panic;
 use std::io::BufRead;
 
-type Input = Vec<String>;
+type Input = Vec<Vec<i64>>;
 
-fn one(_input: &Input) {
+fn is_row_valid(row: &[i64]) -> bool {
+    (row.is_sorted_by(|a, b| a > b) || row.is_sorted_by(|a, b| a < b))
+        && row.windows(2).all(|w| w[0].max(w[1]) - w[1].min(w[0]) <= 3)
+}
+
+fn one(input: &Input) {
     let now = std::time::Instant::now();
-    let sum = 0;
+
+    let sum = input
+        .iter()
+        .filter(|row| {
+            is_row_valid(&row)
+            // // Ensure that the levels are either all decreasing or increasing
+            // if !(row.is_sorted_by(|a, b| a > b) || row.is_sorted_by(|a, b| a < b)) {
+            //     return None;
+            // }
+            // // Ensure that the difference between adjacent levels is at most 3
+            // if !row.windows(2).all(|w| w[0].max(w[1]) - w[1].min(w[0]) <= 3) {
+            //     return None;
+            // }
+            // Some(())
+        })
+        .count();
 
     println!("One: {sum} | Elapsed: {:?}", now.elapsed());
 }
-fn two(_input: &Input) {
+
+fn two(mut input: Input) {
     let now = std::time::Instant::now();
-    let sum = 0;
+
+    let sum = input
+        .iter_mut()
+        .filter(|row| {
+            if is_row_valid(row) {
+                return true;
+            }
+
+            // Bruteforce checking if we can remove any index to make it valid
+            for i in 0..row.len() {
+                let first_half = &row[..i];
+                let second_half = &row[i + 1..];
+
+                if is_row_valid(&[first_half, second_half].concat()) {
+                    return true;
+                }
+            }
+
+            false
+        })
+        .count();
 
     println!("Two: {sum} | Elapsed: {:?}", now.elapsed());
 }
 
 fn parse(input: &[String]) -> Input {
-    input.iter().map(|row| row.to_owned()).collect()
+    input
+        .iter()
+        .map(|row| {
+            row.split_whitespace()
+                .map(|s| s.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect()
 }
 
 fn main() {
@@ -29,5 +78,5 @@ fn main() {
     let input = parse(&input);
 
     one(&input);
-    two(&input);
+    two(input);
 }
