6d460996da22dbebba477432c086751df742d6dd
Mathias Kindberg <mathias@kindberg.earth>
2024-12-02 11:50:52 +0100

    day one done

--- days/aoc_01/src/main.rs
+++ days/aoc_01/src/main.rs
@@ -3,23 +3,53 @@
 
 use std::io::BufRead;
 
-type Input = Vec<String>;
+type Input = (Vec<i64>, Vec<i64>);
 
-fn one(_input: &Input) {
+fn one(input: Input) {
     let now = std::time::Instant::now();
-    let sum = 0;
+
+    let (mut left, mut right) = input;
+    left.sort();
+    right.sort();
+
+    let sum: i64 = left
+        .iter()
+        .zip(right.iter())
+        .map(|(l, r)| l.max(r) - l.min(r))
+        .sum();
 
     println!("One: {sum} | Elapsed: {:?}", now.elapsed());
 }
-fn two(_input: &Input) {
+fn two(input: &Input) {
+    use std::collections::HashMap;
+
     let now = std::time::Instant::now();
-    let sum = 0;
+    let (left, right) = input;
+    let frequencies = right.iter().fold(HashMap::new(), |mut acc, &item| {
+        *acc.entry(item).or_insert(0) += 1;
+        acc
+    });
+
+    let sum: i64 = left
+        .iter()
+        .map(|item| item * *frequencies.get(item).unwrap_or(&0))
+        .sum();
 
     println!("Two: {sum} | Elapsed: {:?}", now.elapsed());
 }
 
 fn parse(input: &[String]) -> Input {
-    input.iter().map(|row| row.to_owned()).collect()
+    use itertools::Itertools;
+
+    input
+        .iter()
+        .map(|row| {
+            row.split("   ")
+                .map(|item| item.parse::<i64>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .collect()
 }
 
 fn main() {
@@ -28,6 +58,6 @@ fn main() {
     let input: Vec<String> = stdin.lock().lines().map_while(Result::ok).collect();
     let input = parse(&input);
 
-    one(&input);
+    one(input.clone());
     two(&input);
 }
